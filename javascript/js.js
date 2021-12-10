94418879c64dc24bed39614cb109d3cd30fbbb44
Helbob <millehelbo@outlook.dk>
2021-12-10 12:36:42 +0100

    burger menu

---
+++ javascript/js.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector("button");
+const ul = document.querySelector("nav");
+
+function toggleMenu() {
+  ul.classList.toggle("shown");
+
+  const menu = ul.classList.contains("shown");
+
+  if (menu) {
+    // hvis ul har klassen "shown"
+    //btn.textContent = "Luk";
+    ul.classList.remove("nav-bar");
+    btn.classList.add("open");
+  } else {
+    // hvis IKKE ul har klassen "shown"
+    //btn.textContent = "Menu";
+    btn.classList.remove("open");
+  }
+}
+
+btn.addEventListener("click", toggleMenu);
